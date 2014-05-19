a66656e81a967b3a2ba58e6dcd3a8291a783f4e5
Chris Howlett <chowlett09@gmail.com>
2014-05-19 16:18:47 +0100

    Functional styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout_from_resource
+
+protected
+  def layout_from_resource
+    devise_controller? ? 'devise' : 'application'
+  end
 end
