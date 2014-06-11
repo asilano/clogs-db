9138760700a75efb25ff56116d86be51f6564280
Chris Howlett <chowlett09@gmail.com>
2014-06-11 14:21:27 +0100

    Fix infinite loop with FactoryGirl

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -2,17 +2,17 @@ require "spec_helper"
 
 describe AdminMailer do
   describe "approval_needed" do
-    let(:user) { FactoryGirl.create(user) }
+    let(:user) { FactoryGirl.create(:user) }
     let(:mail) { AdminMailer.approval_needed(user) }
 
     it "renders the headers" do
-      mail.subject.should eq("User wants approval for Clogs Members")
-      mail.to.should eq(["chowlett09@gmail.com"])
-      mail.from.should eq(["chowlett09@gmail.com"])
+      expect(mail.subject).to eq "User wants approval for Clogs Members"
+      expect(mail.to).to eq ["chowlett09@gmail.com"]
+      expect(mail.from).to eq ["chowlett09@gmail.com"]
     end
 
     it "renders the body" do
-      mail.body.encoded.should match(user.email)
+      expect(mail.body.encoded).to match(user.email)
     end
   end
 
