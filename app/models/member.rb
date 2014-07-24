dc8e613c9bc59d3febc06cf09d68c290bb5c84cd
Chris Howlett <chowlett09@gmail.com>
2014-07-24 12:43:30 +0100

    Set default order on Member - surname, forename

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,8 @@ class Member < ActiveRecord::Base
                   :mailing_list_ids
   has_and_belongs_to_many :mailing_lists
 
+  default_scope order('surname, forename')
+
   def fullname
     "#{forename} #{surname}"
   end
