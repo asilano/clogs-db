2070c861c7a59a9b3dcbf1aa871c21c313efce03
Chris Howlett <chowlett09@gmail.com>
2014-11-07 16:09:06 +0000

    Speculative fix

--- app/models/mailing_list.rb
+++ app/models/mailing_list.rb
@@ -1,8 +1,9 @@
 class MailingList < ActiveRecord::Base
   attr_accessible :name, :member_ids, :query
+  serialize :query, Hash
+
   validates_presence_of :name
   validates_uniqueness_of :name, allow_nil: false
   has_and_belongs_to_many :members
 
-  serialize :query, Hash
 end
