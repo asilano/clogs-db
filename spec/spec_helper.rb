fe17ae0461a9361edd3b9450bb9b997724410e7f
Chris Howlett <chowlett09@gmail.com>
2019-07-22 20:19:54 +0100

    Remove unused capabilities variable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -72,7 +72,7 @@ end
 Capybara.register_driver :firefox_headless do |app|
   options = ::Selenium::WebDriver::Firefox::Options.new
   options.args << '--headless'
-  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options, desired_capabilities: caps)
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 end
 
 Capybara.javascript_driver = :firefox_headless
